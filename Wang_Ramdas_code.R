7da56e65aeced7d720ce565e45f48fb81c69f24f
andreaspzeniou <116413473+andreaspzeniou@users.noreply.github.com>
2023-11-28 19:43:22 -0600

    Added corrected storey

--- Wang_Ramdas_code.R
+++ Wang_Ramdas_code.R
@@ -1,9 +1,9 @@
 #### e-BH #
 
 
-K = 200 #### total number of experiments  
-K0 = 100 ##### number of nulls 
-mu = 4 #### signal strength
+K = 40 #### total number of experiments  
+K0 = 30 ##### number of nulls 
+mu = 2 #### signal strength
 delta = 4  #### martingale construction  
 alpha = c(0.1,0.05,0.02) #### e-BH FDR level 
 penalty = sum(1/(1:K)) ##### penalty paid by BY
@@ -37,20 +37,21 @@ Truedis <- function(TE,alpha,B){
 
 
 
-N=1000  # repeat times!
+N=10000  # repeat times!
 
 start_time <- Sys.time() 
 set.seed(5) 
 
 #GG <- matrix(rep(0, len=15),nrow=5) ### record final results
-HH <- matrix(rep(0, len=48),nrow=8) ### record final results
+HH <- matrix(rep(0, len=99),nrow=11) ### record final results
 
 
 
-colnames(HH) <- c(alpha[1],"FDP%",alpha[2],"FDP%",alpha[3], "FDP%")
-rownames(HH) <- c("BH", "e-BH PRDS", "BY", "e-BH AD", "e-BH", "e-BH Story", "e-BH PRDS Storey", "e-BH AD Storey") 
+colnames(HH) <- c(alpha[1],"FDP%", "PWR", alpha[2],"FDP%", "PWR", alpha[3], "FDP%", "PWR")
+rownames(HH) <- c("BH", "e-BH PRDS", "BY", "e-BH AD", "e-BH", "e-BH Story", "e-BH AD Storey", "e-BH PRDS Storey", "e-BH TS", "e-BH AD TS", "e-BH PRDS TS") 
 
-
+pi_0_vec <- c()
+pi_0_vec2 <- c()
 
 for (h in 1:N)
 {
@@ -94,7 +95,7 @@ for (h in 1:N)
   d=delta
   #boost1<-rep(0,len=3)
   #boost2<-rep(0,len=3)
-  H <- matrix(rep(0,len=48), nrow=8) ### data recorder
+  H <- matrix(rep(0,len=99), nrow=11) ### data recorder
   
   
   for (i in 1:3) {
@@ -134,6 +135,14 @@ for (h in 1:N)
     }
     pi_0_hat <- (1 + k_hat) / length(E)
     
+    beta <- alpha[i] / 10
+    N_e_vals <- length(E)
+    k_hat2 <- length(E[1/E > beta / N_e_vals])
+    pi_0_hat2 <- (k_hat2) / length(E)
+    
+    pi_0_vec[h] <- pi_0_hat
+    pi_0_vec2[h] <- pi_0_hat2
+    
     Eb1=boost1*E
     Eb2=boost2*E 
     
@@ -145,6 +154,9 @@ for (h in 1:N)
     R6 <- ReBH(E,alpha[i] / pi_0_hat)
     R7 <- ReBH(Eb1,alpha[i] / pi_0_hat)
     R8 <- ReBH(Eb2,alpha[i] / pi_0_hat)
+    R9 <- ReBH(E,(alpha[i] - beta) / pi_0_hat2)
+    R10 <- ReBH(Eb1,(alpha[i] - beta) / pi_0_hat2)
+    R11 <- ReBH(Eb2,(alpha[i] - beta) / pi_0_hat2)
     
     TD1 <- Truedis(E,alpha[i],B)
     TD2 <- Truedis(PE,alpha[i],B)
@@ -154,24 +166,45 @@ for (h in 1:N)
     TD6 <- Truedis(E,alpha[i] / pi_0_hat,B)
     TD7 <- Truedis(Eb1,alpha[i] / pi_0_hat,B)
     TD8 <- Truedis(Eb2,alpha[i] / pi_0_hat,B)
+    TD9 <- Truedis(E,(alpha[i] - beta) / pi_0_hat2,B)
+    TD10 <- Truedis(Eb1,(alpha[i] - beta) / pi_0_hat2,B)
+    TD11 <- Truedis(Eb2,(alpha[i] - beta) / pi_0_hat2,B)
     
-    H[1,2*i-1]<-R2
-    H[2,2*i-1]<-R5
-    H[3,2*i-1]<-R3
-    H[4,2*i-1]<-R4
-    H[5,2*i-1]<-R1
-    H[6,2*i-1]<-R6
-    H[7,2*i-1]<-R7
-    H[8,2*i-1]<-R8
+    H[1,3*i-2]<-R2
+    H[2,3*i-2]<-R5
+    H[3,3*i-2]<-R3
+    H[4,3*i-2]<-R4
+    H[5,3*i-2]<-R1
+    H[6,3*i-2]<-R6
+    H[7,3*i-2]<-R7
+    H[8,3*i-2]<-R8
+    H[9,3*i-2]<-R9
+    H[10,3*i-2]<-R10
+    H[11,3*i-2]<-R11
     
-    H[1,2*i]<-((R2-TD2)/max(R2,1))*100
-    H[2,2*i]<-((R5-TD5)/max(R5,1))*100
-    H[3,2*i]<-((R3-TD3)/max(R3,1))*100
-    H[4,2*i]<-((R4-TD4)/max(R4,1))*100
-    H[5,2*i]<-((R1-TD1)/max(R1,1))*100
-    H[6,2*i]<-((R6-TD6)/max(R6,1))*100
-    H[7,2*i]<-((R7-TD7)/max(R7,1))*100
-    H[8,2*i]<-((R8-TD8)/max(R8,1))*100
+    H[1,3*i-1]<-((R2-TD2)/max(R2,1))*100
+    H[2,3*i-1]<-((R5-TD5)/max(R5,1))*100
+    H[3,3*i-1]<-((R3-TD3)/max(R3,1))*100
+    H[4,3*i-1]<-((R4-TD4)/max(R4,1))*100
+    H[5,3*i-1]<-((R1-TD1)/max(R1,1))*100
+    H[6,3*i-1]<-((R6-TD6)/max(R6,1))*100
+    H[7,3*i-1]<-((R7-TD7)/max(R7,1))*100
+    H[8,3*i-1]<-((R8-TD8)/max(R8,1))*100
+    H[9,3*i-1]<-((R9-TD9)/max(R9,1))*100
+    H[10,3*i-1]<-((R10-TD10)/max(R10,1))*100
+    H[11,3*i-1]<-((R11-TD11)/max(R11,1))*100
+    
+    H[1,3*i]<-(TD2 / (max(K - K0,1)))*100
+    H[2,3*i]<-(TD5 / (max(K - K0,1)))*100
+    H[3,3*i]<-(TD3 / (max(K - K0,1)))*100
+    H[4,3*i]<-(TD4 / (max(K - K0,1)))*100
+    H[5,3*i]<-(TD1 / (max(K - K0,1)))*100
+    H[6,3*i]<-(TD6 / (max(K - K0,1)))*100
+    H[7,3*i]<-(TD7 / (max(K - K0,1)))*100
+    H[8,3*i]<-(TD8 / (max(K - K0,1)))*100
+    H[9,3*i]<-(TD9 / (max(K - K0,1)))*100
+    H[10,3*i]<-(TD10 / (max(K - K0,1)))*100
+    H[11,3*i]<-(TD11 / (max(K - K0,1)))*100
   }
   HH = HH+H
 } 
